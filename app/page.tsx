dbcb6f66e6b6448855d976c527e950a487f5fa02
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2024-11-13 07:53:50 +0900

    feat : 레이아웃 개발 중

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-  return <div>page</div>;
+  //https://www.youtube.com/watch?v=-xXASlyU0Ck
+  return (
+    <div className="max-w-3xl mx-auto md:py-10 h-screen">
+      <div className=" h-full border rounded-md flex flex-col relative">
+        <div className="h-20">
+          <div>
+            <div>
+              <h1>Daily Chat</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
