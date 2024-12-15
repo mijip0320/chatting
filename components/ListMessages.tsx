21b28729c3f576f31b282e2e35d7f60af1d72392
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2024-12-16 07:37:34 +0900

    feat : 스크롤 아래로 이동 기능 추가

--- components/ListMessages.tsx
+++ components/ListMessages.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { IMessage, useMessage } from "@/lib/store/messages";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Message from "./Message";
 import { DeleteAlert, EditAlert } from "./MessageActions";
 import { supabaseBrowser } from "@/lib/supabase/browser";
 import { toast } from "sonner";
+import { ArrowDown } from "lucide-react";
 
 export default function ListMessages() {
   const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const [userScroll, setUserScroll] = useState(false);
   //데이터베이스와 상호작용할 액션들
   const {
     messages,
@@ -80,10 +82,21 @@ export default function ListMessages() {
     }
   }, [messages]);
 
+  const handleOnScroll = () => {
+    const scrollContainer = scrollRef.current;
+    if (scrollContainer) {
+      const isScroll =
+        scrollContainer.scrollTop <
+        scrollContainer.scrollHeight - scrollContainer.clientHeight - 10;
+      setUserScroll(isScroll);
+    }
+  };
+
   return (
     <div
       className="flex-1 flex flex-col p-5 h-full overflow-y-auto"
       ref={scrollRef}
+      onScroll={handleOnScroll}
     >
       <div className="flex-1"></div>
       <div className="space-y-7">
@@ -91,6 +104,19 @@ export default function ListMessages() {
           return <Message key={index} message={message} />;
         })}
       </div>
+      {userScroll && (
+        <div className="absolute bottom-20 right-1/2">
+          <div
+            className="w-10 h-10 bg-blue-500 rounded-full justify-center items-center flex mx-auto border cursor-pointer hover:scale-110 transition-all"
+            onClick={() => {
+              scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+            }}
+          >
+            <ArrowDown />
+          </div>
+        </div>
+      )}
+
       <DeleteAlert />
       <EditAlert />
     </div>
